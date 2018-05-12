f6d825fa4b5cbf565241a54706da98fcaa31cde8
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-12 15:57:33 -0400

    Add site.css and basic layout with styling

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -20,15 +20,14 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //Run this first.  Looks for a blank directory URL (at the root of the web server or folder).  Looks for things like default.html, index.html, index.htm.
+            //Changes the internal path to that file.  Then the StaticFiles takes that path and knows what to do with it.
+            //If we used static files first, it would look at the URL and not find anything to serve, and THEN change the setting for default files, but not do anything with it.
+            app.UseDefaultFiles();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            // Add the service of static files as something that the server can do.  By default this looks for files in wwwroot.
+            // wwwroot is the "safe place" for files to host.  Treated as the root of the web server for static/flat files.
+            app.UseStaticFiles();
         }
     }
 }
