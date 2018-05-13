53051b246314de4fa1b2f9d03655030fa8c1a22b
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-13 16:13:44 -0400

    Add logging

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace DutchTreat.Data
 {
@@ -12,25 +13,46 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        //Logger for this class allows us to see where the log came from
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _ctx.Products
-                       .OrderBy(p => p.Title)
-                       .ToList();
+            try
+            {
+                _logger.LogInformation("GetAllProducts");
+
+                return _ctx.Products
+                    .OrderBy(p => p.Title)
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetAllProductsByCategory(string category)
         {
-            return _ctx.Products
-                       .Where(p => p.Category == category)
-                       .OrderBy(p => p.Title)
-                       .ToList();
+            try
+            {
+                return _ctx.Products
+                           .Where(p => p.Category == category)
+                           .OrderBy(p => p.Title)
+                           .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
         public bool SaveAll()
