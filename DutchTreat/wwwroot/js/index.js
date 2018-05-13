59521357d2d83deb0e443f719a09ce2b57399815
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-12 21:43:10 -0400

    Add login form and manipulate with jQuery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,20 +1,29 @@
-﻿var x = 0;
-var s = "";
+﻿//Wrap and execute inside anonymous function to limit scope
+$(document).ready (function() {
+    var x = 0;
+    var s = "";
 
-console.log("Hello Pluralsight");
+    //Hide the form
+    var theForm = $("#theForm"); //Returns a collection that we can execute over
+    theForm.hide();
 
-//Hide the form
-var theForm = $("#theForm"); //Returns a collection that we can execute over
-theForm.hide();
+    //Log that we clicked the buy button
+    var button = $("#buyButton");
+    button.on("click", function() {
+        console.log("Buying Item");
+    });
 
-//Log that we clicked the buy button
-var button = $("#buyButton");
-button.on("click", function() {
-    console.log("Buying Item");
+    //Get list items
+    var listItems = $(".product-props li");
+    listItems.on("click", function() {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function() {
+        $popupForm.fadeToggle(500);
+    });
 });
-
-//Get list items
-var listItems = $(".product-props li");
-listItems.on("click", function() {
-    console.log("You clicked on " + $(this).text());
-})
