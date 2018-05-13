97273c77aebf78b0365384efd875ea4574d91a80
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-12 21:09:34 -0400

    Install jQuery+bootstrap, create index.js

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,20 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hello Pluralsight");
+
+//Hide the form
+var theForm = document.getElementById("theForm");
+
+theForm.hidden = true;
+
+//Log that we buy an item
+var button = document.getElementById("buyButton");
+
+button.addEventListener("click", function() {
+    console.log("Buying Item");
+});
+
+//Get list items
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
