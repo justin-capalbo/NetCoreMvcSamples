1a1c5cf6e11fc0ec86b48390069d42877db6911c
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-13 14:43:08 -0400

    Create entities and dbcontext

---
+++ DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
