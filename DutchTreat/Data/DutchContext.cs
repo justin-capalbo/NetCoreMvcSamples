50e91666b77aaa0334a456925ee3d38954359304
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-13 15:16:56 -0400

    Add EF CLI tools, add config and make 1st migration

--- DutchTreat/Data/DutchContext.cs
+++ DutchTreat/Data/DutchContext.cs
@@ -10,6 +10,8 @@ namespace DutchTreat.Data
 {
     public class DutchContext : DbContext
     {
+        public DutchContext(DbContextOptions<DutchContext> options) : base (options) { }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
     }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,20 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Tell services that we'd like it to use our context.
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                //Get the connection string from configuration.
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            });
+
             //Dependency injection for services!
             //Transient - No data, just methods that do things
             //Scoped    - Services that are more expensive to create, kept around for the length of the connection
