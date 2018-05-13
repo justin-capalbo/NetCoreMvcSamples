4b81fbf6a4de86f10f4a96de233f82d9f697e66e
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-13 10:59:55 -0400

    Merge branch usingMVC

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        } 
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the e-mail
+                _mailService.SendMessage("capalbo.justin@gmail.com", model.Subject, $"From: {model.Name} {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                //Show the errors
+            }
+
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        } 
+    }
+}
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,19 +16,43 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Dependency injection for services!
+            //Transient - No data, just methods that do things
+            //Scoped    - Services that are more expensive to create, kept around for the length of the connection
+            //Singleton - Services that are kept for the lifetime of the server being up 
+            services.AddTransient<IMailService, NullMailService>();
+
+            //TODO - Support for real mail service
+
+            //Needed for dependency injection of MVC when we added UseMvc below.
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //Run this first.  Looks for a blank directory URL (at the root of the web server or folder).  Looks for things like default.html, index.html, index.htm.
-            //Changes the internal path to that file.  Then the StaticFiles takes that path and knows what to do with it.
-            //If we used static files first, it would look at the URL and not find anything to serve, and THEN change the setting for default files, but not do anything with it.
-            app.UseDefaultFiles();
+            //Show developer page when exception is thrown for dev use only
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
 
             // Add the service of static files as something that the server can do.  By default this looks for files in wwwroot.
             // wwwroot is the "safe place" for files to host.  Treated as the root of the web server for static/flat files.
             app.UseStaticFiles();
+
+            //Listen to requests, and see if we can map them to a Controller, which will map them to a View for us.
+            app.UseMvc(cfg =>
+            {
+                //Routes
+                cfg.MapRoute("Default", 
+                    "{controller}/{action}/{id?}", 
+                    new {controller = "App", Action = "Index"});
+            });
         }
     }
 }
