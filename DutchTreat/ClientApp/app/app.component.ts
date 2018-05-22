bf7b448e04d103a36577fb28fd4c5b77172ffeff
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-21 23:33:17 -0400

    Really need to gitignore some of this stuff.
    Added Angular scripts to shop and removed existing html.  Change the
    selector for the component to the-shop, add this special tag to the shop
    view.

--- DutchTreat/ClientApp/app/app.component.ts
+++ DutchTreat/ClientApp/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-wonderful-app',
+  selector: 'the-shop',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- DutchTreat/wwwroot/ClientApp/dist/main.js
+++ DutchTreat/wwwroot/ClientApp/dist/main.js
@@ -41,7 +41,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div style=\"text-align:center\">\n  <h1>\n    Welcome to {{ title }}!\n  </h1>\n  <img width=\"300\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\">\n</div>\n"
+module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div style=\"text-align:center\">\n  <h1>\n    Welcome to {{ title }}!\n  </h1>\n</div>\n"
 
 /***/ }),
 
@@ -69,7 +69,7 @@ var AppComponent = /** @class */ (function () {
     }
     AppComponent = __decorate([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
-            selector: 'my-wonderful-app',
+            selector: 'the-shop',
             template: __webpack_require__(/*! ./app.component.html */ "./ClientApp/app/app.component.html"),
             styles: [__webpack_require__(/*! ./app.component.css */ "./ClientApp/app/app.component.css")]
         })
