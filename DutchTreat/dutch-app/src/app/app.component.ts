d915441cd6b24c9e39433c6156f7ebd8b7df5bb6
Justin Capalbo <capalbo.justin@gmail.com>
2018-05-21 22:43:23 -0400

    Simply ng new dutch-app

---
+++ DutchTreat/dutch-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-wonderful-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Dutch Treat';
+}
